5ce3effe233122e7a50978d970a7b686130ded79
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-09-12 13:13:37 +0900

    地形追加

---
+++ Assets/Debug/Script/Camera.cs
@@ -0,0 +1,35 @@
+// 三人称視点のカメラ処理
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // カメラの向き
+        float horizontal = Input.GetAxis("Horizontal2");
+        float vertical = Input.GetAxis("Vertical2");
+
+        // Y軸回転
+        if(Mathf.Abs(horizontal) > 0.1f)
+        {
+            transform.RotateAround(_player.transform.position, Vector3.up, horizontal);
+        }
+        // X軸回転
+        if(Mathf.Abs(vertical) > 0.1f)
+        {
+            transform.RotateAround(_player.transform.position, transform.right, vertical);
+        }
+    }
+}
--- Assets/Debug/Script/Player.cs
+++ Assets/Debug/Script/Player.cs
@@ -10,11 +10,17 @@ public class Player : MonoBehaviour
     // キャラクターコントローラー
     private CharacterController _characterController;
 
+    [SerializeField] private Camera _camera;
+
     // 進む方向
     private Vector3 _moveDirection;
     private Vector3 _moveX;
     private Vector3 _moveZ;
     private Vector3 _cameraFront;
+
+    // プレイヤーのスピード
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _gravity = 10.0f;
 
     // ゲームパッドスティックの入力状態の変数
     private float _horizontal;
@@ -52,13 +58,20 @@ public class Player : MonoBehaviour
     {
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
-        //_cameraFront = Vector3.Scale()
-        //_moveZ
 
-        //_moveDirection = _horizontal + _vertical;
+        // カメラの向きを基準にした正面方向のベクトル
+        _cameraFront = Vector3.Scale(_camera.transform.forward, new Vector3(1.0f, 0.0f, 1.0f)).normalized;
+        // 移動
+        _moveZ = _cameraFront * _vertical * _speed;
+        _moveX = _camera.transform.right * _horizontal * _speed;
 
-        transform.LookAt(transform.position);
-        _characterController.Move(_moveDirection);
+        _moveDirection = _moveZ + _moveX + new Vector3(0.0f, _moveDirection.y, 0.0f);
+        _moveDirection.y -= _gravity * Time.deltaTime;
+
+        // プレイヤーの進む方向に向きを変更
+        transform.LookAt(transform.position + _moveZ + _moveX);
+        // 
+        _characterController.Move(_moveDirection * Time.deltaTime);
 
         //_rigidbody.AddForce(_horizontal, 0, _vertical, ForceMode.Force);
     }
