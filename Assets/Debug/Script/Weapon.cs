5db56059e7bd835a5c80e398993aa32dbe693738
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-09-19 10:32:09 +0900

    動きはしないが弾は生成

--- Assets/Debug/Script/Weapon.cs
+++ Assets/Debug/Script/Weapon.cs
@@ -8,13 +8,19 @@ public class Weapon : MonoBehaviour
     // 武器オブジェクト
     [SerializeField] private GameObject _weaponObject;
     // 弾発射位置のオブジェクト
-    [SerializeField] private GameObject _fireBulletObject;
+    [SerializeField] private GameObject _fireBulletPositionObject;
+    // 弾
+    [SerializeField] private GameObject _bulletObject;
 
+    // 弾が消える時間
+    private float _disappearBulletTime;
+
+    // 生成したかどうか
+    private bool _isGenerated;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -23,10 +29,23 @@ public class Weapon : MonoBehaviour
         // トリガーの入力状態取得
         float testTrigger = Input.GetAxis("L_R_Trigger");
 
-        if(testTrigger != 0)
-        {
+        disappearBullet();
 
+
+        if (!Player._instance._isHoldWeapon) return;
+        // 弾発射
+        if(testTrigger >= 0.5)
+        {
+            if (_isGenerated) return;
+            Instantiate(_bulletObject, _fireBulletPositionObject.transform.position, Quaternion.identity);
+            _isGenerated = true;
         }
+        else if(testTrigger == 0)
+        {
+            _isGenerated=false;
+        }
+
+
 
     }
 
@@ -41,5 +60,20 @@ public class Weapon : MonoBehaviour
             _weaponObject.SetActive(false);
         }
         
+    }
+
+
+    private void disappearBullet()
+    {
+        // 消える時間をカウント中
+        _disappearBulletTime = Time.deltaTime;
+
+        Debug.Log(_disappearBulletTime);
+
+        if(_disappearBulletTime >= 5)
+        {
+            Destroy(_bulletObject);
+            _disappearBulletTime=0;
+        }
     }
 }
