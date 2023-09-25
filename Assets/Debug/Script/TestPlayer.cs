ecf2d03631e1b4807a802b8058f82ac6de00f887
Hmmtrnt <rento5310@gmail.com>
2023-09-26 02:00:34 +0900

    プレイヤーをキャラクターコントローラーからRigidbodyに変更途中

--- Assets/Debug/Script/TestPlayer.cs
+++ Assets/Debug/Script/TestPlayer.cs
@@ -4,32 +4,13 @@ using UnityEngine;
 
 public class TestPlayer : MonoBehaviour
 {
-    // リジットボディ
+    // プレイヤーのRigidbody.
     private Rigidbody _rigidbody;
-
-    // 移動速度
-    [SerializeField] private Vector3 _velocity;
-    // 入力値
-    private Vector3 _input;
-
-    // 地面と判定するレイヤーマスク
-    [SerializeField] private LayerMask _groundLayers;
-
-    // 速さ
-    [SerializeField] private float _speed = 4.0f;
-
-    // 接地しているかどうか
-    [SerializeField] private bool _ifGrounded;
-
-    // 接地確認のコライダの位置のオフセット
-    [SerializeField] private Vector3 _groundPositionOffset = new Vector3(0.0f, 0.02f, 0.0f);
-    // 接地確認の球のコライダの半径
-    [SerializeField] private float groundColliderRadius = 0.29f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -37,4 +18,5 @@ public class TestPlayer : MonoBehaviour
     {
         
     }
+
 }
