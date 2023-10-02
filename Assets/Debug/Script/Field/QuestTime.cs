a1760c4d81910be70cdd5bf2a17b0526d465ba17
Hmmtrnt <rento5310@gmail.com>
2023-10-03 00:27:59 +0900

    時計の処理追加

--- Assets/Debug/Script/Field/QuestTime.cs
+++ Assets/Debug/Script/Field/QuestTime.cs
@@ -8,13 +8,15 @@ public class QuestTime : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localEulerAngles = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        int m = DateTime.Now.Minute;
-        GetComponent<Transform>().localEulerAngles = new Vector3 (0,0,-360 / 60.0f * m);
+        Time.timeScale = 1.0f / 60.0f;
+        float m = Time.time;
+        GameObject.Find("LongHand").transform.localEulerAngles = new Vector3 (0,0,-360 / 60.0f * m);
+        GameObject.Find("HourHand").transform.localEulerAngles = new Vector3(0, 0, -360 / 60.0f * 50.0f);
     }
 }
