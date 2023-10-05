4d790a311658a38f083cc4615e87fae6ec1152f6
Hmmtrnt <rento5310@gmail.com>
2023-10-06 00:18:24 +0900

    結局ステートパターンはわからなかった

--- Assets/Debug/Script/Player/PlayerStatePattern.cs
+++ Assets/Debug/Script/Player/PlayerStatePattern.cs
@@ -1,7 +1,10 @@
+using StatePattern;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 namespace StatePattern
 {
@@ -58,18 +61,22 @@ namespace StatePattern
         void IState.OnStateChanged()
         {
             // 初期化
+            Debug.Log("アイドルチェンジ");
         }
 
         void IState.OnStateBegin()
         {
             IsEndState = false;
+            Debug.Log("アイドルはじめ");
         }
         void IState.OnStateEnd()
         {
             IsEndState = true;
+            Debug.Log("アイドル終了");
         }
         void IState.Update(float deltaTime)
         {
+            Debug.Log("アイドル");
             if (IsEndState) return;
             if(m_nextState != null)
             {
@@ -103,20 +110,111 @@ namespace StatePattern
         StateType IState.GetCurrentState { get; } = StateType.RUNNING;
         bool IState.ChangeState(IState nextState)
         {
+            Debug.Log("CangeState");
             IState state = this;
             state.OnStateEnd();
             if(nextState == null) return false;
 
             nextState.OnStateChanged();
             nextState.OnStateBegin();
+            return true;
         }
 
+        void IState.OnStateChanged()
+        {
+            // Initialize
+            Debug.Log("走るチェンジ");
+        }
+        void IState.OnStateBegin() 
+        {
+            IsEndState = false;
+            Debug.Log("走るはじめ");
+        }
+        void IState.OnStateEnd()
+        {
+            IsEndState = true;
+            Debug.Log("走る終了");
+        }
+        void IState.Update(float deltaTime) 
+        {
+            Debug.Log("走る");
+
+            if (IsEndState) return;
+            if(m_nextState != null)
+            {
+                (this as IState).ChangeState(m_nextState);
+                return;
+            }
+
+            // 左スティック情報取得
+            //_horizontalLeftStick = ControllerManager._inctance._LeftStickHorizontal;
+            //_verticalLeftStick = ControllerManager._inctance._LeftStickVertical;
+
+            //// カメラの正面ベクトル
+            //Vector3 cameraForward = Vector3.Scale(_camera.transform.forward, new Vector3(1.0f, 0.0f, 1.0f)).normalized;
+
+            //// カメラの向きでVectorの咆哮を変える
+            //Vector3 verticalDirection = cameraForward * _verticalLeftStick * _moveSpeed;// 前後
+            //Vector3 horizontalDirection = _camera.transform.right * _horizontalLeftStick * _moveSpeed;// 左右
+
+            //// ダッシュ
+            //if (Stamina._instance.GetCurrentLengthGauge() <= Stamina._instance.GetLastGauge() &&
+            //   ControllerManager._inctance._RBButton)
+            //{
+            //    _moveSpeed = 4.0f;
+            //}
+            //else if (ControllerManager._inctance._RBButton)
+            //{
+            //    _moveSpeed = 15.0f;
+            //}
+            //else if (!ControllerManager._inctance._RBButton)
+            //{
+            //    _moveSpeed = 7.0f;
+            //}
+
+            //// 重力
+            //_moveDirection.y = _gravity;
+
+            //// 移動する方向
+            //_moveDirection = verticalDirection + horizontalDirection + new Vector3(0.0f, _moveDirection.y, 0.0f);
+
+            //// 移動する向き
+            //transform.LookAt(_transform.position + verticalDirection + horizontalDirection);
+
+
+            //// 移動
+            //_characterController.Move(_moveDirection * Time.deltaTime);
+
+
+            //if (m_moveTarget == null) return;
+            //Vector3 currentPosition = m_moveTarget.position;
+            //Vector3 moveVec = (m_moveDestination - currentPosition).normalized;
+            //m_moveTarget.position = currentPosition + moveVec * (m_moveSpeed * deltaTime);
+
+        }
+
+        void IState.SetNextState(IState nextState) { m_nextState = nextState; }
+        IState IState.GetNextState() {  return m_nextState; }
+
         #endregion// ====IState====
+
+        public void SetTarget(Transform target, Vector3 destination, float speedPerSec)
+        {
+            m_moveTarget = target;
+            m_moveDestination = destination;
+            m_moveSpeed = speedPerSec;
+        }
     }
 }
 
 public class PlayerStatePattern : MonoBehaviour
 {
+    private IState currentState = new IdleState();
+
+    // ゲームパッドの入力状態
+    private float _horizontalLeftStick;
+    private float _verticalLeftStick;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -126,6 +224,17 @@ public class PlayerStatePattern : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontalLeftStick = ControllerManager._inctance._LeftStickHorizontal;
+        _verticalLeftStick = ControllerManager._inctance._LeftStickVertical;
+
+        currentState.Update(Time.deltaTime);
+        //Debug.Log("あ");
+
+        if(_horizontalLeftStick != 0 ||
+            _verticalLeftStick != 0)
+        {
+            Debug.Log("チェンジ");
+            //currentState.SetNextState(MoveState);
+        }
     }
 }
