8494d9e61fceea62c40f5c1f182f70ba0b1a65fd
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-10-06 16:46:54 +0900

    ステートパターン追加途中

--- Assets/Debug/Script/Player/startPaturnTest/PlayerStateSample.cs
+++ Assets/Debug/Script/Player/startPaturnTest/PlayerStateSample.cs
@@ -8,9 +8,38 @@ public partial class PlayerStateSample
     private static readonly StateRunning _running = new StateRunning();
     private static readonly StateDead _dead = new StateDead();
 
+
     /// <summary>
     /// 現在のState
     /// </summary>
     private PlayerStateBase _currentState = _idle;
+
+    public bool IsDead => _currentState is StateDead;
+
+    // Start()から呼ばれる
+    private void OnStart()
+    {
+        _currentState.OnEnter(this, null);
+    }
+
+    private void OnUpdate()
+    {
+        _currentState.OnUpdate(this);
+    }
+
+    // ステート変更
+    private void ChangeState(PlayerStateBase nextState)
+    {
+        _currentState.OnExit(this, nextState);
+        nextState.OnEnter(this, _currentState);
+        _currentState = nextState;
+    }
+
+    // 死亡した時に呼ばれる
+    private void OnDeath()
+    {
+        ChangeState(_dead);
+    }
+
 
 }
