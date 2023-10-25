2b21903062dc561b14bc5429094d8f016778f7d6
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-10-25 19:01:02 +0900

    シーンマネージャー追加

---
+++ Assets/Game/Debug/Scenes/DebugSceneScript/DebugSceneTransition.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugSceneTransition : MonoBehaviour
+{
+    public void DebugPlayScene()
+    {
+        SceneTransitionManager.LoadScene("DebugPlayScene");
+    }
+
+    public void TitleScene()
+    {
+        SceneTransitionManager.LoadScene("TitleScene");
+    }
+
+    public void MainScene()
+    {
+        SceneTransitionManager.LoadScene("MainScene");
+    }
+
+    public void ResultScene()
+    {
+        SceneTransitionManager.LoadScene("ResultScene");
+    }
+}
---
+++ Assets/Game/Script/SceneTransitionManager.cs
@@ -0,0 +1,15 @@
+﻿/*シーンマネージャー*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    // タイトルシーン遷移
+    public void TitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+
+}
